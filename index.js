f2c6e7723e8be5f7acbbb41542dc521eee025f77
Ayon <ayonjodder177@gmail.com>
2022-04-30 09:41:12 +0600

    Delete user done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 app.use(cors());
 app.use(express.json())
@@ -251,18 +252,30 @@ async function run() {
     try {
         await client.connect();
         const collection = client.db("foodExpress").collection("user");
-        // const user = { name: 'AJ', email: 'jd@gmail.com' };
-        // const result = await collection.insertOne(user)
-        // console.log(`user insurted with id ${result.insertedId}`);
-
-        app.post('/user', (req, res) => {
+        //Get API----------------->
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const users = await cursor.toArray();
+            res.send(users)
+        })
+        //Post API---------------->
+        app.post('/user', async (req, res) => {
             const newUser = req.body;
             console.log('Adding new user', newUser);
-            res.send({result: 'success'})
+            const result = await collection.insertOne(newUser)
+            res.send(result)
+        })
+        //Delet API---------------->
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await collection.deleteOne(query);
+            res.send(result)
         })
     }
     finally {
-        await client.close()
+        // await client.close()
     }
 }
 run().catch(console.dir)
